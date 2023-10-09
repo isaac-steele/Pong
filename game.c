f08c6d7c3d3f296ff420455f29bd30ec5aaa6be4
Kade Lindsay <kli107@uclive.ac.nz>
2023-10-09 15:30:55 +1300

    Framework for paddle display and movement

--- game.c
+++ game.c
@@ -1,10 +1,15 @@
 #include "system.h"
+#include "display.h "
+#include "pacer.h"
+
+
 
 int main (void)
 {
     system_init ();
-
-
+    paddle_init();
+    ball_init();
+    
     while (1)
     {
 
---
+++ paddle.c
@@ -0,0 +1,21 @@
+/** @file paddle.c
+    @author K. Lindsay
+    @date 9 August 2023
+    @brief Initialize and move the paddle 
+*/
+
+
+#include "system.h"
+#include "tinygl.h"
+
+/** Initialize the paddle */
+void paddle_init()
+{
+
+}
+
+
+int main(void)
+{
+    
+}
