12b90a8f97d999e38efeb125c7ee03e53c3cfc4d
Kade Lindsay <kli107@uclive.ac.nz>
2023-10-09 16:04:10 +1300

    header file for paddle.c with basic functions

---
+++ paddle.h
@@ -0,0 +1,16 @@
+/** @file paddle.c
+    @author K. Lindsay
+    @date 9 August 2023
+    @brief Paddle support
+*/
+
+#ifndef PADDLE_H
+#define PADDLE_H
+
+#include "system.h"
+
+void paddle_init(void);
+void paddle_move_left(void);
+void paddle_move_right(void);
+
+#endif /* PACER_H*/
