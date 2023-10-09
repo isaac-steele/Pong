40f6c7de41d6074bcb757c079a575b89fb186332
Kade Lindsay <kli107@uclive.ac.nz>
2023-10-09 16:02:21 +1300

    Added paddle.h header and tinygl

--- game.c
+++ game.c
@@ -1,18 +1,25 @@
 #include "system.h"
-#include "display.h "
 #include "pacer.h"
+#include "tinygl.h"
 
+#include "paddle.h"
+    
 
+#define LOOP_RATE 1000
 
 int main (void)
 {
+
     system_init ();
+    tinygl_init(LOOP_RATE);
+    pacer_init(LOOP_RATE);
+
     paddle_init();
-    ball_init();
-    
+
+
     while (1)
     {
-
-
+        pacer_wait();
+        tinygl_update();
     }
 }
