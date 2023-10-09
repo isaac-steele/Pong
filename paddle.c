caeea9a0e4546bf4a2cf49f7b3158aae7d290bb0
Kade Lindsay <kli107@uclive.ac.nz>
2023-10-09 16:03:21 +1300

    Created paddle file with empty move and init func

--- paddle.c
+++ paddle.c
@@ -7,15 +7,21 @@
 
 #include "system.h"
 #include "tinygl.h"
+#include "pacer.h"
+
 
 /** Initialize the paddle */
 void paddle_init()
 {
+    tinygl_drawline(tinygl_point(0,2), tinygl_point(0,5))
+}
+
+void paddle_move_left()
+{
 
 }
 
-
-int main(void)
+void paddle_move_right()
 {
     
 }
