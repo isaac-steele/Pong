9a8e79785278aa4c909ebb3a1b252111a58103b6
ist46 <ist46@uclive.ac.nz>
2023-10-12 11:41:49 +1300

    flashing thr ball

--- game.c
+++ game.c
@@ -13,13 +13,15 @@ int main (void)
 
     system_init ();
 
-    ball_state_t ball = ball_init (3,3, DIR_E);
+    ball_state_t ball = ball_init (3,3, DIR_SW);
     
     tinygl_init(LOOP_RATE);
     pacer_init(LOOP_RATE);
     
     Paddle_pos_t paddle = paddle_init();
     navswitch_init();
+
+    int tick = 0;
 
     while (1)
     {
@@ -35,5 +37,17 @@ int main (void)
         }
 
         tinygl_draw_point(ball.pos, 1);
+
+        tick++;
+
+        if (tick >= 40) {
+            tick = 0;
+
+            tinygl_draw_point(ball.pos, 0);
+
+            ball = ball_update(ball, paddle);
+
+            tinygl_draw_point(ball.pos, 1);
+        }
     }
 }
