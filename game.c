f0daa94499e6225ef83daa9960c6f7e2796f1a4a
ist46 <ist46@uclive.ac.nz>
2023-10-16 22:43:31 +1300

    got the scoring and ir working

--- game.c
+++ game.c
@@ -10,7 +10,7 @@
 #include "ball.h"
 #include "setup.h"
 
-#define INITIAL_SCORE (char) 0
+#define INITIAL_SCORE  0
 #define START_MODE 0
 #define PLAY_MODE 1
 #define DISPLAY_SCORE 2
@@ -18,8 +18,8 @@
 
 /* game state, contains all key information about current game state*/
 typedef struct {
-    char score;
-    char opponent_score;
+    uint8_t score;
+    uint8_t opponent_score;
     uint8_t mode; //game modes: SETUP==0, PLAY == 1, DISPLAY_SCORE == 2
     bool has_ball;
 } Game_state_t;
@@ -61,7 +61,9 @@ void do_ball_stuff(Paddle_t* paddle, ball_state_t* ball, Game_state_t* game)
         tinygl_draw_point(ball->pos, 0);
         *ball = ball_update(*ball, *paddle);
         if(ball->pos.x == POINT_SCORED ) {
-            //game->opponent_score++;
+            game->opponent_score++;
+            //TODO end the game
+            //TODO correct the trajectory of the ball
             tinygl_clear();
             game->mode = DISPLAY_SCORE;
         }
@@ -121,11 +123,9 @@ int main (void)
     game.opponent_score = INITIAL_SCORE;
     game.mode = 0;
 
-    led_init();
-    led_set(LED1, 0);
     uint32_t tick = 0;
 
-   
+    char scores[6] = "012345";
 
 
     while(1) {
@@ -147,11 +147,20 @@ int main (void)
                 }
                 break;
             case DISPLAY_SCORE:
-                led_set(LED1, 1);
-                char buffer[2];
-                buffer[0] = game.opponent_score;
-                buffer[1] = '\0';
-                tinygl_text (buffer);
+                navswitch_update ();
+                if(navswitch_push_event_p (NAVSWITCH_PUSH)){
+                    tinygl_clear();
+                    game.mode = PLAY_MODE;
+                    paddle = paddle_init();
+                    ball = ball_init (1,5, DIR_SE);
+                    tinygl_draw_point(ball.pos, 1);
+                    game.has_ball = true;
+                } else {
+                    char buffer[2];
+                    buffer[0] = scores[game.opponent_score];
+                    buffer[1] = '\0';
+                    tinygl_text (buffer);
+                }
                 break;
 
         }
